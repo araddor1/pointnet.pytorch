fdc4f22e7dac01c456f4be97152be0e7317a85b0
dorarad <araddor1@gmail.com>
2021-04-22 20:43:41 +0300

    lightning_solution added - seems great!

---
+++ lightning_solution/model.py
@@ -0,0 +1,247 @@
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+import pytorch_lightning as pl
+import torch.nn.parallel
+import torch.utils.data
+from torch.autograd import Variable
+import numpy as np
+import torch.nn.functional as F
+
+class STN3d(pl.LightningModule):
+    def __init__(self):
+        super(STN3d, self).__init__()
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 9)
+        self.relu = nn.ReLU()
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
+        if x.is_cuda:
+            iden = iden.cuda()
+        x = x + iden
+        x = x.view(-1, 3, 3)
+        return x
+    
+    def configure_optimizers(self):
+        optimizer = optim.Adam(classifier.parameters(), lr=0.001, betas=(0.9, 0.999))
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+        return optimizer ,scheduler
+
+
+class STNkd(pl.LightningModule):
+    def __init__(self, k=64):
+        super(STNkd, self).__init__()
+        self.conv1 = torch.nn.Conv1d(k, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, k*k)
+        self.relu = nn.ReLU()
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+        self.k = k
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+
+        x = F.relu(self.bn4(self.fc1(x)))
+        x = F.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        iden = Variable(torch.from_numpy(np.eye(self.k).flatten().astype(np.float32))).view(1,self.k*self.k).repeat(batchsize,1)
+        if x.is_cuda:
+            iden = iden.cuda()
+        x = x + iden
+        x = x.view(-1, self.k, self.k)
+        return x
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(classifier.parameters(), lr=0.001, betas=(0.9, 0.999))
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+        return optimizer ,scheduler
+
+class PointNetfeat(pl.LightningModule):
+    def __init__(self, global_feat = True, feature_transform = False):
+        super(PointNetfeat, self).__init__()
+        self.stn = STN3d()
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 128, 1)
+        self.conv3 = torch.nn.Conv1d(128, 1024, 1)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.global_feat = global_feat
+        self.feature_transform = feature_transform
+        if self.feature_transform:
+            self.fstn = STNkd(k=64)
+
+    def forward(self, x):
+        n_pts = x.size()[2]
+        trans = self.stn(x)
+        x = x.transpose(2, 1)
+        x = torch.bmm(x, trans)
+        x = x.transpose(2, 1)
+        x = F.relu(self.bn1(self.conv1(x)))
+
+        if self.feature_transform:
+            trans_feat = self.fstn(x)
+            x = x.transpose(2,1)
+            x = torch.bmm(x, trans_feat)
+            x = x.transpose(2,1)
+        else:
+            trans_feat = None
+
+        pointfeat = x
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+        if self.global_feat:
+            return x, trans, trans_feat
+        else:
+            x = x.view(-1, 1024, 1).repeat(1, 1, n_pts)
+            return torch.cat([x, pointfeat], 1), trans, trans_feat
+    
+    def configure_optimizers(self):
+        optimizer = optim.Adam(classifier.parameters(), lr=0.001, betas=(0.9, 0.999))
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+        return optimizer ,scheduler
+
+#### main NET
+class PointNetCls(pl.LightningModule):
+    def __init__(self, k=2, feature_transform=None,show_points_func=None):
+        super(PointNetCls, self).__init__()
+        self.feature_transform = feature_transform
+        self.feat = PointNetfeat(global_feat=True, feature_transform=feature_transform)
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, k)
+        self.dropout = nn.Dropout(p=0.3)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.relu = nn.ReLU()
+        self.show_points_func = show_points_func
+
+    def forward(self, x):
+        x, trans, trans_feat = self.feat(x)
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.dropout(self.fc2(x))))
+        x = self.fc3(x)
+        return F.log_softmax(x, dim=1), trans, trans_feat
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.001, betas=(0.9, 0.999))
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+        return [optimizer] , [scheduler]
+
+
+    def training_step(self, batch, batch_idx):
+        points, target = batch
+        target = target[:, 0]
+        if(batch_idx % 20 == 0):
+            self.show_points_func(points[0].cpu().numpy())
+        points = points.transpose(2, 1)
+        pred, trans, trans_feat = self(points)
+        loss = F.nll_loss(pred, target)
+        if self.feature_transform is not None:
+            loss += self.feature_transform(trans_feat) * 0.001
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        total_correct = 0
+        total_testset = 0
+        points, target = batch
+        target = target[:, 0]
+        points = points.transpose(2, 1)
+        points, target = points, target
+        pred, _, _ = classifier(points)
+        pred_choice = pred.data.max(1)[1]
+        correct = pred_choice.eq(target.data).sum()
+        total_correct += correct.item()
+        total_testset += points.size()[0]
+        loss = total_correct / float(total_testset)
+        print("final accuracy {}".format(loss))
+        return loss
+
+
+
+
+class PointNetDenseCls(pl.LightningModule):
+    def __init__(self, k = 2, feature_transform=False):
+        super(PointNetDenseCls, self).__init__()
+        self.k = k
+        self.feature_transform=feature_transform
+        self.feat = PointNetfeat(global_feat=False, feature_transform=feature_transform)
+        self.conv1 = torch.nn.Conv1d(1088, 512, 1)
+        self.conv2 = torch.nn.Conv1d(512, 256, 1)
+        self.conv3 = torch.nn.Conv1d(256, 128, 1)
+        self.conv4 = torch.nn.Conv1d(128, self.k, 1)
+        self.bn1 = nn.BatchNorm1d(512)
+        self.bn2 = nn.BatchNorm1d(256)
+        self.bn3 = nn.BatchNorm1d(128)
+
+    def forward(self, x):
+        batchsize = x.size()[0]
+        n_pts = x.size()[2]
+        x, trans, trans_feat = self.feat(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = self.conv4(x)
+        x = x.transpose(2,1).contiguous()
+        x = F.log_softmax(x.view(-1,self.k), dim=-1)
+        x = x.view(batchsize, n_pts, self.k)
+        return x, trans, trans_feat
+    
+    def configure_optimizers(self):
+        optimizer = optim.Adam(classifier.parameters(), lr=0.001, betas=(0.9, 0.999))
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+        return optimizer ,scheduler
+
+
+def feature_transform_regularizer(trans):
+    d = trans.size()[1]
+    batchsize = trans.size()[0]
+    I = torch.eye(d)[None, :, :]
+    if trans.is_cuda:
+        I = I.cuda()
+    loss = torch.mean(torch.norm(torch.bmm(trans, trans.transpose(2,1)) - I, dim=(1,2)))
+    return loss
---
+++ lightning_solution/trainClsLight.py
@@ -0,0 +1,96 @@
+from __future__ import print_function
+import argparse
+import os
+import random
+import torch
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+from pointnet.dataset import ShapeNetDataset, ModelNetDataset
+from lighting_solution.model import PointNetCls, feature_transform_regularizer
+import torch.nn.functional as F
+import pytorch_lightning as pl
+from tqdm import tqdm
+
+def train():
+    from utils.show3d_balls import showpoints
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--batchSize', type=int, default=32, help='input batch size')
+    parser.add_argument(
+        '--num_points', type=int, default=100, help='input batch size')
+    parser.add_argument(
+        '--workers', type=int, help='number of data loading workers', default=4)
+    parser.add_argument(
+        '--nepoch', type=int, default=250, help='number of epochs to train for')
+    parser.add_argument('--outf', type=str, default='cls', help='output folder')
+    parser.add_argument('--model', type=str, default='', help='model path')
+    parser.add_argument('--dataset', type=str, required=True, help="dataset path")
+    parser.add_argument('--dataset_type', type=str, default='shapenet', help="dataset type shapenet|modelnet40")
+    parser.add_argument('--feature_transform', action='store_true', help="use feature transform")
+
+    opt = parser.parse_args()
+    print(opt)
+
+    blue = lambda x: '\033[94m' + x + '\033[0m'
+
+    opt.manualSeed = random.randint(1, 10000)  # fix seed
+    print("Random Seed: ", opt.manualSeed)
+    random.seed(opt.manualSeed)
+    torch.manual_seed(opt.manualSeed)
+
+    if opt.dataset_type == 'shapenet':
+        dataset = ShapeNetDataset(
+            root=opt.dataset,
+            classification=True,
+            npoints=opt.num_points)
+
+        test_dataset = ShapeNetDataset(
+            root=opt.dataset,
+            classification=True,
+            split='test',
+            npoints=opt.num_points,
+            data_augmentation=False)
+    elif opt.dataset_type == 'modelnet40':
+        dataset = ModelNetDataset(
+            root=opt.dataset,
+            npoints=opt.num_points,
+            split='trainval')
+
+        test_dataset = ModelNetDataset(
+            root=opt.dataset,
+            split='test',
+            npoints=opt.num_points,
+            data_augmentation=False)
+    else:
+        exit('wrong dataset type')
+
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=opt.batchSize,
+        shuffle=True,
+        num_workers=int(opt.workers))
+
+    testdataloader = torch.utils.data.DataLoader(
+            test_dataset,
+            batch_size=opt.batchSize,
+            shuffle=True,
+            num_workers=int(opt.workers))
+
+    print(len(dataset), len(test_dataset))
+    num_classes = len(dataset.classes)
+    print('classes', num_classes)
+
+    try:
+        os.makedirs(opt.outf)
+    except OSError:
+        pass
+
+    model = PointNetCls(k=num_classes,feature_transform=feature_transform_regularizer,show_points_func=showpoints)
+    trainer =  pl.Trainer(gpus=1)
+    trainer.fit(model,dataloader,testdataloader)
+
+
+if __name__ == "__main__":
+    train()
